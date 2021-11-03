55b3d109981b1ebbdf15b26cadf03770cc0b19ab
meltoroun <adsd3>
2021-11-04 02:04:52 +0300

    adding files

---
+++ script.js
@@ -0,0 +1,19 @@
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.slider-next').addEventListener('click', function(){
+    offset = offset + 854;
+    if (offset > 2562) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    offset = offset - 854;
+    if (offset < 0) {
+        offset = 2562;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
