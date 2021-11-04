d2a19cc384824569a6e18b4cd5c08a1377e622f9
meltoroun <adsd3>
2021-11-04 04:24:59 +0300

    ternary operator

--- script.js
+++ script.js
@@ -2,18 +2,12 @@ let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 
 document.querySelector('.slider-next').addEventListener('click', function(){
-    offset = offset + 854;
-    if (offset > 2562) {
-        offset = 0;
-    }
-    sliderLine.style.left = -offset + 'px';
+    (offset > 2570) ? offset = 0 : sliderLine.style.left = -offset + 'px';
+    offset += 854;
 });
 
 document.querySelector('.slider-prev').addEventListener('click', function () {
-    offset = offset - 854;
-    if (offset < 0) {
-        offset = 2562;
-    }
-    sliderLine.style.left = -offset + 'px';
+    (offset < 0) ? offset = 2570 : sliderLine.style.left = -offset + 'px';
+    offset -= 854;
 });
 
